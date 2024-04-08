2e381e68b76feaa3848252864fb666904c137b98
carol savchick <carolmariehinds@gmail.com>
2024-04-07 23:01:44 -0400

    completed caesar cipher

--- cipher.py
+++ cipher.py
@@ -1,3 +1,18 @@
-# add your code here
-#test
-print(test)
+text = input("please enter a sentence: ")
+shift = 5
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char not in alphabet:
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print("Please enter a sentence: ", message)
+    print("THe encrypted sentence is: ", encrypted_text)
+
+caesar(text, shift)                
